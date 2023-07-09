56b56f5262d206a02e9ec4ffc3bca30650945b72
Rodolfo Neto <rodolfoneto@gmail.com>
2023-07-09 10:21:02 -0700

    feat add: Validation for connectionSettings, cargoShipSettings, supplySettings, buildSettings

--- src/Domain/Validation/ParamsValidation.php
+++ src/Domain/Validation/ParamsValidation.php
@@ -11,13 +11,83 @@ class ParamsValidation
 
     public function validation(object $params)
     {
+        $this->validateConnectionSettings($params->connectionSettings ?? null);
+        
         $this->validateSpeedUpSettings($params->speedUpSettings ?? null);
         $this->validateGatherSettings($params->gatherSettings ?? null);
         $this->validateRallySettings($params->rallySettings ?? null);
+        $this->validateCargoShipSettings($params->cargoShipSettings ?? null);
+        $this->validateSupplySettings($params->supplySettings ?? null);
 
         $this->validateHeroSettings($params->heroSettings ?? null);
         $this->validateHeroStageSettings($params->heroStageSettings ?? null);
         $this->validateArenaSettings($params->arenaSettings ?? null);
+        $this->validateBuildSettings($params->buildSettings ?? null);
+    }
+
+    protected function validateSupplySettings($supplySettings)
+    {
+        $this->validateBoolean($supplySettings->sendResources, 'supplySettings->sendResources');
+        $this->validateBoolean($supplySettings->useBagResource, 'supplySettings->useBagResource');
+        $this->validateBoolean($supplySettings->randomizeSpeed, 'supplySettings->randomizeSpeed');
+        
+        $this->validateFloat($supplySettings->supplySpeed, 'supplySettings->supplySpeed');
+        $this->validateInteger($supplySettings->maxTravelTime, 'supplySettings->maxTravelTime');
+        $this->validateString($supplySettings->playerToSend, 'supplySettings->playerToSend');
+
+        $this->validateIntegerArray($supplySettings->reservedRss, 5, 'supplySettings->reservedRss');
+        $this->validateIntegerArray($supplySettings->supplyMin, 5, 'supplySettings->supplyMin');
+        $this->validateIntegerArray($supplySettings->reservedBagRss, 5, 'supplySettings->reservedBagRss');
+        
+        $this->validateBooleanArray($supplySettings->typesToSend, 5, 'supplySettings->typesToSend');
+        $this->validateBooleanArray($supplySettings->bagTypesToSend, 5, 'supplySettings->bagTypesToSend');
+    }
+
+    protected function validateCargoShipSettings($cargoShipSettings)
+    {
+        $this->validateBoolean($cargoShipSettings->allowTrading, 'cargoShipSettings->allowTrading');
+        $this->validateBoolean($cargoShipSettings->tradeFood, 'cargoShipSettings->tradeFood');
+        $this->validateBoolean($cargoShipSettings->tradeStone, 'cargoShipSettings->tradeStone');
+        $this->validateBoolean($cargoShipSettings->tradeWood, 'cargoShipSettings->tradeWood');
+        $this->validateBoolean($cargoShipSettings->tradeOre, 'cargoShipSettings->tradeOre');
+        $this->validateBoolean($cargoShipSettings->tradeGold, 'cargoShipSettings->tradeGold');
+        $this->validateBoolean($cargoShipSettings->ignoreFood, 'cargoShipSettings->ignoreFood');
+        $this->validateBoolean($cargoShipSettings->ignoreStone, 'cargoShipSettings->ignoreStone');
+        $this->validateBoolean($cargoShipSettings->ignoreWood, 'cargoShipSettings->ignoreWood');
+        $this->validateBoolean($cargoShipSettings->ignoreOre, 'cargoShipSettings->ignoreOre');
+        $this->validateBoolean($cargoShipSettings->ignoreGold, 'cargoShipSettings->ignoreGold');
+        $this->validateBoolean($cargoShipSettings->ignoreAnima, 'cargoShipSettings->ignoreAnima');
+        $this->validateBoolean($cargoShipSettings->ignoreLunite, 'cargoShipSettings->ignoreLunite');
+        $this->validateBoolean($cargoShipSettings->ignoreSpeedUp, 'cargoShipSettings->ignoreSpeedUp');
+        $this->validateBoolean($cargoShipSettings->exchangeRssItemOnly, 'cargoShipSettings->exchangeRssItemOnly');
+        $this->validateBoolean($cargoShipSettings->useRssFromBagIfNeeded, 'cargoShipSettings->useRssFromBagIfNeeded');
+        
+        $this->validateInteger($cargoShipSettings->exchangeMinQuality, 'cargoShipSettings->exchangeMinQuality');
+    }
+
+    protected function validateConnectionSettings($connectionSettings)
+    {
+        $this->validateString($connectionSettings->savedProxy, 'connectionSettings.savedProxy');
+        
+        $this->validateBoolean($connectionSettings->isTaiwan, 'connectionSettings.isTaiwan');
+        $this->validateBoolean($connectionSettings->saveLogToFile, 'connectionSettings.saveLogToFile');
+        $this->validateBoolean($connectionSettings->dontClearLog, 'connectionSettings.dontClearLog');
+        $this->validateBoolean($connectionSettings->refreshKey, 'connectionSettings.refreshKey');
+        $this->validateBoolean($connectionSettings->resetConnection, 'connectionSettings.resetConnection');
+        $this->validateBoolean($connectionSettings->postToWebhook, 'connectionSettings.postToWebhook');
+        $this->validateBoolean($connectionSettings->postKeyExpiry, 'connectionSettings.postKeyExpiry');
+        $this->validateBoolean($connectionSettings->postStatusChange, 'connectionSettings.postStatusChange');
+        $this->validateBoolean($connectionSettings->postScheduleChange, 'connectionSettings.postScheduleChange');
+        $this->validateBoolean($connectionSettings->postOtherLogin, 'connectionSettings.postOtherLogin');
+        $this->validateBoolean($connectionSettings->postRSSLimit, 'connectionSettings.postRSSLimit');
+        
+        $this->validateInteger($connectionSettings->reconnectTime, 'connectionSettings.reconnectTime');
+        $this->validateInteger($connectionSettings->otherLoginTime, 'connectionSettings.otherLoginTime');
+        $this->validateInteger($connectionSettings->workerSpeed, 'connectionSettings.workerSpeed');
+        $this->validateInteger($connectionSettings->cmdInterval, 'connectionSettings.cmdInterval');
+        $this->validateInteger($connectionSettings->resetTime, 'connectionSettings.resetTime');
+        $this->validateInteger($connectionSettings->resetTimeBegin, 'connectionSettings.resetTimeBegin');
+        $this->validateInteger($connectionSettings->logResetTime, 'connectionSettings.logResetTime');
     }
 
     public function validateGatherSettings($gatherSettings)
@@ -131,6 +201,20 @@ class ParamsValidation
     }
 
 
+    protected function validateBuildSettings($buildSettings)
+    {
+        $this->validateBoolean($buildSettings->autoBuild, 'buildSettings->autoBuild');
+        $this->validateBoolean($buildSettings->buildByLowestLevel, 'buildSettings->buildByLowestLevel');
+        $this->validateBoolean($buildSettings->ignoreSpamTarget, 'buildSettings->ignoreSpamTarget');
+
+        $this->validateInteger($buildSettings->buildPriority, 'buildSettings->buildPriority');
+        $this->validateInteger($buildSettings->maxBuildLevel, 'buildSettings->maxBuildLevel');
+        $this->validateInteger($buildSettings->spamTarget, 'buildSettings->spamTarget');
+        
+        $this->validateIntegerArrayWithoutSize($buildSettings->buildTarget, 'buildSettings->buildTarget');
+    }
+
+
     public function validateNull($value, $nameField)
     {
         if (is_null($value)) {
@@ -138,10 +222,24 @@ class ParamsValidation
         }
     }
 
+    protected function validateString($value, $nameField)
+    {
+        if (!is_string($value)) {
+            throw new ParamsFormatException("\"{$nameField}\" must be a string value.");
+        }
+    }
+
     protected function validateInteger($value, $nameField)
     {
         if (!is_int($value)) {
             throw new ParamsFormatException("\"{$nameField}\" must be an integer value.");
+        }
+    }
+
+    protected function validateFloat($value, $nameField)
+    {
+        if (!is_numeric($value)) {
+            throw new ParamsFormatException("\"{$nameField}\" must be an float value.");
         }
     }
 
@@ -172,6 +270,34 @@ class ParamsValidation
         foreach($array as $item) {
             if (!is_bool($item)) {
                 throw new ParamsFormatException("\"{$nameField}\" must be an array of booleans");
+            }
+        }
+    }
+
+    protected function validateIntegerArray(array $array, int $size, $nameField = "")
+    {
+        if (!is_array($array)
+            || count($array) !== $size
+        ) {
+            throw new ParamsFormatException("\"{$nameField}\" must be an array with {$size} positions");
+        }
+
+        foreach($array as $item) {
+            if (!is_int($item)) {
+                throw new ParamsFormatException("\"{$nameField}\" must be an array of integer");
+            }
+        }
+    }
+
+    protected function validateIntegerArrayWithoutSize($arrayInteger, $nameField)
+    {
+        if (!is_array($arrayInteger)) {
+            throw new ParamsFormatException("\"{$nameField}\" must be an array");
+        }
+
+        foreach($arrayInteger as $item) {
+            if (!is_int($item)) {
+                throw new ParamsFormatException("\"{$nameField}\" must be an array of integer");
             }
         }
     }
